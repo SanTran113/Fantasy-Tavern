3f235ac42bd54b7b3f7729a994192e97dcf4a27b
SanTran113 <stran113@calpoly.edu>
2024-11-05 10:48:30 -0800

    got render to work

--- packages/proto/public/scripts/inventoryProfileElement.js
+++ packages/proto/public/scripts/inventoryProfileElement.js
@@ -6,112 +6,110 @@ export class InvenProfileElement extends HTMLElement {
     return this.getAttribute("src");
   }
 
-
-    static template = html `
+  static template = html`
     <template>
-        <main class="mainInventory">
-            <div class="profile">profile</div>
-            <div class="InventoryTitle">
-                <div class="Invtitle">Inventory</div>
-            </div>
-            <div class="Inventory">
-                <slot name="inventory"><div>Item 1</div></slot>
-            </div>
-            <div class="userName"><slot name="name">Name</slot></div>
-            <div class="class"><slot name="userClass">Class</slot></div>
-        </main>
+      <main class="mainInventory">
+        <div class="profile">profile</div>
+        <div class="InventoryTitle">
+          <div class="Invtitle">Inventory</div>
+        </div>
+        <div class="Inventory">
+          <slot name="inventory"><div>Item 1</div></slot>
+        </div>
+        <div class="userName"><slot name="name">Name</slot></div>
+        <div class="class"><slot name="userClass">Class</slot></div>
+      </main>
     </template>
-    `;
+  `;
 
-    static styles = css`
+  static styles = css`
     :host {
       display: contents;
     }
 
     .mainInventory {
-    background-image: url('../assets/Inventory/inventoryPageBg.png');
-    padding: 5%;
-    background-size: 100% 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    aspect-ratio: 4/2;
+      background-image: url("../assets/Inventory/inventoryPageBg.png");
+      padding: 5%;
+      background-size: 100% 100%;
+      background-position: center;
+      background-repeat: no-repeat;
+      aspect-ratio: 4/2;
 
-    display: grid;
-    grid-template-columns: repeat(15, 1fr);
-    grid-template-rows: repeat(7, 1fr);
-    gap: 10px;
-    grid-auto-rows: minmax(100px, auto);
+      display: grid;
+      grid-template-columns: repeat(15, 1fr);
+      grid-template-rows: repeat(7, 1fr);
+      gap: 10px;
+      grid-auto-rows: minmax(100px, auto);
     }
 
     .profile {
-    grid-column: 4 / span 3;
-    grid-row: 2 / span 3;
-    background-image: url('../assets/Inventory/profileTavern.png');
-    background-size: 100% 100%;
-    background-position: center;
-    background-repeat: no-repeat;
+      grid-column: 4 / span 3;
+      grid-row: 2 / span 3;
+      background-image: url("../assets/Inventory/profileTavern.png");
+      background-size: 100% 100%;
+      background-position: center;
+      background-repeat: no-repeat;
     }
 
     .Inventory {
-    background-image: url('../assets/Inventory/inventorybg.png');
-    grid-column: 8 / span 5;
-    grid-row: 3 / span 4;
-    background-size: 100% 100%;
-    background-position: center;
-    background-repeat: no-repeat;
+      background-image: url("../assets/Inventory/inventorybg.png");
+      grid-column: 8 / span 5;
+      grid-row: 3 / span 4;
+      background-size: 100% 100%;
+      background-position: center;
+      background-repeat: no-repeat;
 
-    padding: 5%;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: repeat(3, 1fr);
-    gap: 3%;
-    grid-auto-rows: minmax(100px, auto);
+      padding: 5%;
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      grid-template-rows: repeat(3, 1fr);
+      gap: 3%;
+      grid-auto-rows: minmax(100px, auto);
     }
 
     .InventoryTitle {
-    background-image: url('../assets/Inventory/InventoryTitle.png');
-    grid-column: 8 / span 5;
-    grid-row: 2 / span 1;
-    background-size: 100% 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    display: flex;
-    align-items: center;
-    justify-content: center; 
-    color: white;
-    text-align: center;
+      background-image: url("../assets/Inventory/InventoryTitle.png");
+      grid-column: 8 / span 5;
+      grid-row: 2 / span 1;
+      background-size: 100% 100%;
+      background-position: center;
+      background-repeat: no-repeat;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: white;
+      text-align: center;
     }
 
     .Invtitle {
-
     }
 
     .userName {
-    background-image: url('../assets/Inventory/userClassBg.png');
-    grid-column: 4 / span 3;
-    grid-row: 5 / span 1;
-    background-size: 100% 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    display: flex;
-    align-items: center;
-    justify-content: center; 
-    color: white;
-    text-align: center;
+      background-image: url("../assets/Inventory/userClassBg.png");
+      grid-column: 4 / span 3;
+      grid-row: 5 / span 1;
+      background-size: 100% 100%;
+      background-position: center;
+      background-repeat: no-repeat;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: white;
+      text-align: center;
     }
 
     .class {
-    background-image: url('../assets/Inventory/userClassBg.png');
-    grid-column: 4 / span 3;
-    grid-row: 6 / span 1;
-    background-size: 100% 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    display: flex;
-    align-items: center;
-    justify-content: center; 
-    color: white;
-    text-align: center;
+      background-image: url("../assets/Inventory/userClassBg.png");
+      grid-column: 4 / span 3;
+      grid-row: 6 / span 1;
+      background-size: 100% 100%;
+      background-position: center;
+      background-repeat: no-repeat;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: white;
+      text-align: center;
     }
   `;
 
@@ -133,18 +131,23 @@ export class InvenProfileElement extends HTMLElement {
         return res.json();
       })
       .then((json) => this.renderSlots(json))
-      .catch((error) =>
-        console.log(`Failed to render data ${url}:`, error)
-      );
+      .catch((error) => console.log(`Failed to render data ${url}:`, error));
   }
 
   renderSlots(json) {
-  const entries = Object.entries(json);
-  const toSlot = ([key, value]) =>
-    html`<span slot="${key}">${value}</span>`
-
-  const fragment = entries.map(toSlot);
-  this.replaceChildren(...fragment);
-}
-
+    const entries = Object.entries(json);
+    const toSlot = ([key, value]) => {
+      switch (typeof value) {
+        case "object":
+          if (Array.isArray(value))
+            return html` ${value.map(
+              (s) => html`<span slot="${key}">${s}</span>`
+            )}`;
+        default:
+          return html`<span slot="${key}">${value}</span>`;
+      }
+    };
+    const fragment = entries.map(toSlot);
+    this.replaceChildren(...fragment);
+  }
 }
