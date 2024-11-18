0361c9ccbb35a7b28b7f67c2c44d403498bdbf16
SanTran113 <stran113@calpoly.edu>
2024-11-17 22:20:20 -0800

    changed header

--- packages/proto/public/scripts/header.js
+++ packages/proto/public/scripts/header.js
@@ -15,77 +15,17 @@ import {
       "mu-dropdown": Dropdown.Element
     });
   
-    static template = html`<template>
-      <header>
-        <h1>Blazing Travels</h1>
-        <nav>
-          <p><slot> Unnamed Tour </slot></p>
-          <mu-dropdown>
-            <a slot="actuator">
-              Hello,
-              <span id="userid"></span>
-            </a>
-            <menu>
-              <li>
-                <label class="dark-mode-switch">
-                  <input type="checkbox" />
-                  Dark Mode
-                </label>
-              </li>
-              <li class="when-signed-in">
-                <a id="signout">Sign Out</a>
-              </li>
-              <li class="when-signed-out">
-                <a href="/login">Sign In</a>
-              </li>
-            </menu>
-          </mu-dropdown>
-        </nav>
-      </header>
-    </template>`;
-  
-    static styles = css`
-      :host {
-        display: contents;
-      }
-      header {
-        display: flex;
-        flex-wrap: wrap;
-        align-items: bottom;
-        justify-content: space-between;
-        padding: var(--size-spacing-medium);
-        background-color: var(--color-background-header);
-        color: var(--color-text-inverted);
-      }
-      header ~ * {
-        margin: var(--size-spacing-medium);
-      }
-      header p {
-        --color-link: var(--color-link-inverted);
-      }
-      nav {
-        display: flex;
-        flex-direction: column;
-        flex-basis: max-content;
-        align-items: end;
-      }
-      a[slot="actuator"] {
-        color: var(--color-link-inverted);
-        cursor: pointer;
-      }
-      #userid:empty::before {
-        content: "traveler";
-      }
-      menu a {
-        color: var(--color-link);
-        cursor: pointer;
-        text-decoration: underline;
-      }
-      a:has(#userid:empty) ~ menu > .when-signed-in,
-      a:has(#userid:not(:empty)) ~ menu > .when-signed-out {
-        display: none;
-      }
+    static template = html`
+      <template>
+        <div id="userid"></div>
+        <button id="signout">Sign Out</button>
+        <label class="dark-mode-switch">
+          <input type="checkbox" /> Dark Mode
+        </label>
+      </template>
     `;
+  
+    static styles = css``;
   
     get userid() {
       return this._userid.textContent;
@@ -94,8 +34,10 @@ import {
     set userid(id) {
       if (id === "anonymous") {
         this._userid.textContent = "";
+        this._signout.disabled = true;
       } else {
         this._userid.textContent = id;
+        this._signout.disabled = false;
       }
     }
   
@@ -105,13 +47,14 @@ import {
         .template(HeaderElement.template)
         .styles(
           reset.styles,
-          headings.styles,
+          // headings.styles,
           HeaderElement.styles
         );
   
-      const dm = this.shadowRoot.querySelector(
-        ".dark-mode-switch"
-      );
+      this._userid = this.shadowRoot.querySelector("#userid");
+      this._signout = this.shadowRoot.querySelector("#signout");
+  
+      const dm = this.shadowRoot.querySelector(".dark-mode-switch input");
   
       dm.addEventListener("click", (event) =>
         Events.relay(event, "dark-mode", {
@@ -119,15 +62,12 @@ import {
         })
       );
   
-      this._userid = this.shadowRoot.querySelector("#userid");
-      this._signout = this.shadowRoot.querySelector("#signout");
-  
       this._signout.addEventListener("click", (event) =>
         Events.relay(event, "auth:message", ["auth/signout"])
       );
     }
-  
-    _authObserver = new Observer(this, "blazing:auth");
+    
+    _authObserver = new Observer(this, "main:auth");
   
     connectedCallback() {
       this._authObserver.observe(({ user }) => {
