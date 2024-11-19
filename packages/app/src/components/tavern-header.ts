021e2efb485162e88c58df25127d7d37f5a994b5
SanTran113 <stran113@calpoly.edu>
2024-11-19 10:51:17 -0800

    event handler in element for lit

--- packages/app/src/components/tavern-header.ts
+++ packages/app/src/components/tavern-header.ts
@@ -1,19 +1,37 @@
+import { Auth, define, Dropdown, Events, Observer } from "@calpoly/mustang";
 import { LitElement, css, html } from "lit";
+
+function toggleDarkMode(ev: InputEvent) {
+  const target = ev.target as HTMLInputElement;
+  const checked = target.checked;
+
+  Events.relay(ev, "dark-mode", { checked });
+}
 
 export class TavernHeaderElement extends LitElement {
   render() {
     return html`
       <header>
-        <label
-          class="darkmodeButton"
-          onchange="toggleDarkMode(event.target, event.target.checked)"
-        >
-          <input type="checkbox" autocomplete="off" />
-          Dark mode
+        <label @change=${toggleDarkMode}>
+          <input type="checkbox" />
+          Dark Mode
         </label>
       </header>
     `;
   }
 
   static styles = css``;
+
+  // static initializeOnce() {
+  //   function toggleDarkMode(page: HTMLElement, checked: boolean) {
+  //     page.classList.toggle("dark-mode", checked);
+  //   }
+
+  //   document.body.addEventListener("dark-mode", (event) =>
+  //     toggleDarkMode(
+  //       event.currentTarget as HTMLElement,
+  //       (event as CustomEvent).detail?.checked
+  //     )
+  //   );
+  // }
 }
